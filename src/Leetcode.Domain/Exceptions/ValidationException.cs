52534ee218499c7ed0074f8bf102e1cab4704878
Sardor <sardorstudent0618@gmail.com>
2024-04-20 06:15:29 +0500

    Domain and Infrastructure Layer

---
+++ src/Leetcode.Domain/Exceptions/ValidationException.cs
@@ -0,0 +1,10 @@
+﻿namespace Leetcode.Domain.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException() { }
+
+        public ValidationException(string message)
+            : base(message) { }
+    }
+}
